ec03c3b8681f307352fc7a20e2fbcb226230980b
jack17432 <jack17432@gmail.com>
2025-03-22 19:03:21 +1300

    start working on the menu

--- monarchs/src/lib.rs
+++ monarchs/src/lib.rs
@@ -1,1 +1,11 @@
+use bevy::prelude::States;
 
+pub mod start_menu;
+
+#[derive(States, Default, Clone, Debug, PartialEq, Eq, Hash)]
+pub enum GameState {
+    #[default]
+    StartMenu,
+    Running,
+    Paused,
+}
--- monarchs/src/main.rs
+++ monarchs/src/main.rs
@@ -1,7 +1,13 @@
 use bevy::prelude::*;
+use bevy_egui::EguiPlugin;
+use monarchs::GameState;
+use monarchs::start_menu::StartMenuPlugin;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugins(EguiPlugin)
+        .init_state::<GameState>()
+        .add_plugins(StartMenuPlugin)
         .run();
 }
