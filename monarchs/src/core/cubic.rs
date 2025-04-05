421d42aa9193270c46607f3643b7f742c40c01b4
Jack17432 <Jack17432@gmail.com>
2025-04-05 22:41:16 +1300

    Start work on render pipeline for world

--- monarchs/src/core/cubic.rs
+++ monarchs/src/core/cubic.rs
@@ -1,4 +1,6 @@
+use std::collections::HashMap;
 use bevy::prelude::*;
+use bevy::render::extract_resource::ExtractResource;
 
 #[derive(Default, Debug)]
 pub struct CubicPlugin;
@@ -6,3 +8,11 @@ pub struct CubicPlugin;
 impl Plugin for CubicPlugin {
     fn build(&self, _app: &mut App) {}
 }
+
+#[derive(Default, Debug, Clone)]
+pub struct Block {
+    id: u32,
+}
+
+#[derive(Default, Debug, Resource, ExtractResource, Clone)]
+pub struct WorldBlocks(pub HashMap<(isize, isize), Block>);
--- monarchs/src/views/graphics/render_blocks.rs
+++ monarchs/src/views/graphics/render_blocks.rs
@@ -1,8 +1,63 @@
+use bevy::core_pipeline::core_3d::Opaque3d;
+use bevy::ecs::query::ROQueryItem;
+use bevy::ecs::system::SystemParamItem;
+use crate::core::cubic;
 use bevy::prelude::*;
+use bevy::render::extract_resource::ExtractResourcePlugin;
+use bevy::render::render_phase::{AddRenderCommand, RenderCommand, RenderCommandResult, SetItemPipeline, TrackedRenderPass};
+use bevy::render::render_resource::{RenderPipelineDescriptor, SpecializedRenderPipeline, SpecializedRenderPipelines};
+use bevy::render::RenderApp;
+
+const BLOCK_SHADER: &str = "shaders/block.vert";
 
 #[derive(Debug, Default)]
 pub struct RenderBlocksPlugin;
 
 impl Plugin for RenderBlocksPlugin {
-    fn build(&self, _app: &mut App) {}
+    fn build(&self, app: &mut App) {
+        app.add_plugins(ExtractResourcePlugin::<cubic::WorldBlocks>::default());
+
+        app.get_sub_app_mut(RenderApp)
+            .unwrap()
+            .init_resource::<RenderBlocksPipeline>()
+            .init_resource::<SpecializedRenderPipelines<RenderBlocksPipeline>>()
+            .add_render_command::<Opaque3d, DrawBlocksPhaseItemCommands>();
+    }
+}
+
+#[derive(Debug, Resource)]
+struct RenderBlocksPipeline {
+    shader: Handle<Shader>,
+}
+
+impl FromWorld for RenderBlocksPipeline {
+    fn from_world(world: &mut World) -> Self {
+        let asset_server = world.resource::<AssetServer>();
+
+        Self {
+            shader: asset_server.load(BLOCK_SHADER).into(),
+        }
+    }
+}
+
+impl SpecializedRenderPipeline for RenderBlocksPipeline {
+    type Key = ();
+
+    fn specialize(&self, key: Self::Key) -> RenderPipelineDescriptor {
+        todo!()
+    }
+}
+
+type DrawBlocksPhaseItemCommands = (SetItemPipeline, DrawBlocksPhaseItem);
+
+struct DrawBlocksPhaseItem;
+
+impl<P> RenderCommand<P> for DrawBlocksPhaseItem {
+    type Param = ();
+    type ViewQuery = ();
+    type ItemQuery = ();
+
+    fn render<'w>(item: &P, view: ROQueryItem<'w, Self::ViewQuery>, entity: Option<ROQueryItem<'w, Self::ItemQuery>>, param: SystemParamItem<'w, '_, Self::Param>, pass: &mut TrackedRenderPass<'w>) -> RenderCommandResult {
+        todo!()
+    }
 }
